00d2ba4876d2896f3dcb0c0605023039aa598a7d
Jairo Orostegui <jorostogui1@udi.edu.co>
2023-02-28 07:56:15 -0500

    creando componentes compartidos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CabeceraComponent } from './compartidos/cabecera/cabecera.component';
+import { PieceraComponent } from './compartidos/piecera/piecera.component';
+import { ConfirmarComponent } from './compartidos/confirmar/confirmar.component';
+import { MenudenavegacionComponent } from './compartidos/menudenavegacion/menudenavegacion.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CabeceraComponent,
+    PieceraComponent,
+    ConfirmarComponent,
+    MenudenavegacionComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/compartidos/menudenavegacion/menudenavegacion.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { MenudenavegacionComponent } from './menudenavegacion.component';
+
+describe('MenudenavegacionComponent', () => {
+  let component: MenudenavegacionComponent;
+  let fixture: ComponentFixture<MenudenavegacionComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ MenudenavegacionComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(MenudenavegacionComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
