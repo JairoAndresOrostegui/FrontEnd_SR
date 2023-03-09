7b83bcac095b386ecfa2bd137a0959f667edbbaf
Jairo Orostegui <jorostogui1@udi.edu.co>
2023-03-09 15:49:48 -0500

    ajustes a consultar unidad

--- src/app/administrativos/direccionacademica/consultarunidad/consultarunidad.component.ts
+++ src/app/administrativos/direccionacademica/consultarunidad/consultarunidad.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-consultarunidad',
@@ -7,9 +8,62 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ConsultarunidadComponent implements OnInit {
 
-  constructor() { }
+  frmbuscar: FormGroup;
+  frmactualizar: FormGroup;
+  cmbtipoespacio: any;
+  cmbtipoespaciodep: any;
+  cmbunidaddependencia: any;
+  cmbdptounidad: any;
+  cmbmunicipiound: any;
+  visible: boolean;
+  mostrar: boolean;
+  chequeo: boolean;
+  txtformulario: string;
+  urlimagen: string;
+
+  constructor(private fb: FormBuilder) {
+    this.txtformulario = 'Actualizar';
+    this.visible = false;
+    this.mostrar = false;
+    this.chequeo = false;
+    this.urlimagen = "";
+    this.frmbuscar = this.fb.group ({ entrada: [''], filtro: ['nombre'] });
+    this.frmactualizar = this.fb.group ({
+      nombre: ['', Validators.required],
+      piso: [0, Validators.required],
+      capacidad: [0, Validators.required],
+      cantidad: [0, Validators.required],
+      tipoespacio: [''],
+      unidaddependencia: [''],
+      municipiound: [''],
+      dptounidad: [''],
+      caracteristica: [''],
+      tipoespaciodep: [''],
+      estado: ['']
+    })
+  }
 
   ngOnInit(): void {
+  }
+
+  selectMunicipio(): void {
+
+  }
+
+  selectipoespaciodep(): void {
+
+  }
+
+  buscarUnidad(): void {
+
+  }
+
+  mostrarUnidad(id: number): void {
+
+  }
+
+  actualizarUnidad(): void {
+
   }
 
   agregarCaracteristica(): void {
@@ -17,10 +71,6 @@ export class ConsultarunidadComponent implements OnInit {
   }
 
   eliminarCaracteristica(): void {
-
-  }
-
-  actualizarUnidad(): void {
 
   }
 
