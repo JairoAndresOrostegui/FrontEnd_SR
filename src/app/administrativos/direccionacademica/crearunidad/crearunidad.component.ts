b77b4eb52489f6193cdcdd801434d03d94abce91
JairoAndresOrostegui <Jairoo314@hotmail.com>
2023-03-13 17:56:16 -0500

    migrando crear unidad

--- src/app/administrativos/direccionacademica/crearunidad/crearunidad.component.ts
+++ src/app/administrativos/direccionacademica/crearunidad/crearunidad.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
+import { RestService } from 'src/app/servicios/rest.service';
 
 @Component({
   selector: 'app-crearunidad',
@@ -7,9 +10,59 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CrearunidadComponent implements OnInit {
 
-  constructor() { }
+  crearsede: FormGroup;
+  cmbtipoespacio: any;
+  cmbdptounidad: any;
+  cmbmunicipiound: any;
+  cmbcaracteristica: any;
+  cmbtipoespaciodep: any;
+  cmbunidaddependencia: any;
+  caracteristicaslista: any;
+  caracteristicaspush: any;
+  chequeo = false;
+  valido = false;
+  urlimagen = '';
+
+  constructor(
+    private peticion: RestService,
+    private fb: FormBuilder,
+    private toastr: ToastrService,
+  ) {
+    this.caracteristicaslista = [];
+    this.caracteristicaspush = [];
+    this.crearsede = this.fb.group ({ 
+      nombre: ['', Validators.required],
+      piso: [0, Validators.required],
+      capacidad: [0, Validators.required],
+      cantidad: [0, Validators.required],
+      tipoespacio: [''],
+      unidaddependencia: [''],
+      municipiound: [''],
+      dptounidad: [''],
+      caracteristica: [''],
+      tipoespaciodep: ['']
+    });
+  }
 
   ngOnInit(): void {
+  }
+
+  selectipoespaciodep(): void {/*
+    setTimeout(() => {
+      this.peticion.getselect('unidadorganizacional/combo/'+this.crearsede.value.tipoespaciodep).subscribe((respuesta) => {
+        this.cmbunidaddependencia = respuesta;
+        this.crearsede.controls['unidaddependencia'].setValue(this.cmbunidaddependencia[0].value);
+      });
+    }, 100);*/
+  }
+ 
+  selectMunicipio(): void {/*
+    setTimeout(() => {
+      this.peticion.getselect('municipio/'+this.crearsede.value.dptounidad).subscribe((respuesta) => {
+        this.cmbmunicipiound = respuesta;
+        this.crearsede.controls['municipiound'].setValue(this.cmbmunicipiound[0].value);
+      });
+    }, 100);*/
   }
 
 }
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,25 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CrearreservaComponent } from './administrativos/direccionacademica/crearreserva/crearreserva.component';
 import { IniciarsesionComponent } from './autenticacion/iniciarsesion/iniciarsesion.component';
+import { ConsultarreservaComponent } from './administrativos/direccionacademica/consultarreserva/consultarreserva.component';
+import { ActualizarreservaComponent } from './administrativos/direccionacademica/actualizarreserva/actualizarreserva.component';
+import { CrearunidadComponent } from './administrativos/direccionacademica/crearunidad/crearunidad.component';
+import { TipounidadComponent } from './administrativos/direccionacademica/tipounidad/tipounidad.component';
+import { CaracteristicasunidadComponent } from './administrativos/direccionacademica/caracteristicasunidad/caracteristicasunidad.component';
+import { InformesreservaComponent } from './administrativos/direccionacademica/informesreserva/informesreserva.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/iniciarsesion', pathMatch: 'full'},
   { path: 'iniciarsesion', component: IniciarsesionComponent},
+  { path: 'consultarreserva', component: ConsultarreservaComponent},
   { path: 'crearreserva', component: CrearreservaComponent},
+  { path: 'actualizarreserva', component: ActualizarreservaComponent},
   { path: 'consultarunidad', component: ConsultarunidadComponent},
+  { path: 'crearunidad', component: CrearunidadComponent},
+  { path: 'tipounidad', component: TipounidadComponent},
+  { path: 'caracteristicaunidad', component: CaracteristicasunidadComponent},
+  { path: 'informe1', component: InformesreservaComponent},
+  { path: 'informe2', component: InformesreservaComponent},
   { path: '**', redirectTo: '', pathMatch: 'full'}
 ];
 
