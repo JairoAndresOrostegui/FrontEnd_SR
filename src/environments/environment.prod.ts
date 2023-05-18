40cee6ff53e03bafdda509593503aca82aac67b1
JairoAndresOrostegui <Jairoo314@hotmail.com>
2023-05-18 16:14:42 -0500

    ajustando captcha

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,5 +3,6 @@ export const environment = {
     siteKey: '6LdtqaohAAAAAMc6nVdLyjTYuaCUDr25nKPq5u4u',
   },
   production: true,
-  apiUrl: 'http://190.217.58.171:44347/api/'
+  apiUrl: 'http://190.217.58.171:44347/api/',
+  apirUrlNestor: 'http://localhost:44347/api/'
 };
